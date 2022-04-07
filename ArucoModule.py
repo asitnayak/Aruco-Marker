35a70603082aa096fed9ac480aed410222fe8970
asitnayak <asitworld123@gmail.com>
2022-04-08 01:53:28 +0530

    doc-string added

--- ArucoModule.py
+++ ArucoModule.py
@@ -4,6 +4,11 @@ import os
 
 
 def loadImgAug(path):
+    """
+    :param path:folder in which all the marker images with ids are stored
+    :return: dictionary with key as the id and values as the augment image
+    """
+
     myList = os.listdir(path)
     noOfMarkers = len(myList)
     print(f"Total number of markers detected : {noOfMarkers}")
@@ -16,6 +21,14 @@ def loadImgAug(path):
 
 
 def findArucoMarkers(img, markerSize=6, totalMarkers=250, draw=True):
+    """
+    :param img: image in which to find the aruco markers
+    :param markerSize: the size of the markers
+    :param totalMarkers: total number of markers that compose the dictionary
+    :param draw: flag to draw bbox around markers detected
+    :return: bounding boxes and id numbers of markers detected
+    """
+
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     key = getattr(cv.aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
     arucoDict = cv.aruco.Dictionary_get(key)
@@ -29,6 +42,15 @@ def findArucoMarkers(img, markerSize=6, totalMarkers=250, draw=True):
 
 
 def augmentAruco(bbox, id, img, imgAug, drawId=True):
+    """
+    :param bbox: the four corner points of the box
+    :param id: marker id of the corresponding box used only for display
+    :param img: the final image on which to draw
+    :param imgAug: the image that wil  be overlapped on the marker
+    :param drawId: flag to display the id of the detected markers
+    :return: image with the augment image overlaid
+    """
+
     
     tl = bbox[0][0][0], bbox[0][0][1]
     tr = bbox[0][1][0], bbox[0][1][1]
